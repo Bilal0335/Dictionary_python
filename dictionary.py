69bbbc81cc0890063ca4a86e09463b8b774f2b83
Muhammad Bilal Hussain <94027068+Bilal0335@users.noreply.github.com>
2023-09-29 22:11:45 +0500

    dictionary python

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dict={"List":"A collection of mutable object ",
+      "Tuple":"A collection of immutable object",
+      "Set":"A collection of non repeatable or unique object",
+      "Dictionary":"A collection of Key and it's value",
+      }
+
+Word = input("enter words\n")
+print("Meaning of the word is : ", dict[Word])
